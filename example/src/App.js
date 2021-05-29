a99d16ab2c8bb48d5d44539d48120c0bee2bcd1d
Dawid Dębowski <dawid199960@gmail.com>
2021-05-29 20:34:03 +0200

    update Read.me file

--- example/src/App.js
+++ example/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react'
 
 import { JitsiComponent } from 'e-stella-jitsi'
-import 'e-stella-jitsi/dist/index.css'
 
 const App = () => {
   return <JitsiComponent displayName="displayName" roomName="roomName" admin={true} />
